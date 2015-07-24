dd7d84ba87a14effd22da07feb039d82c5a444e0
Justin Barber <barber.justin@gmail.com>
2015-07-23 21:05:14 -0400

    meta

--- grape_juice.rb
+++ grape_juice.rb
@@ -4,16 +4,18 @@ class GrapeJuice < Grape::API
   format :json
   version 'v1', using: :path
 
-  resource :users do
-    get do
-      { :action => 'index' }
-    end
+  [:users, :statuses].each do |api_resource|
+    resource api_resource do
+      get do
+        { :action => "#{api_resource} index" }
+      end
 
-    params do
-      requires :id, type: Integer, desc: "User id."
-    end
-    get ":id" do
-      { :action => 'read' }
+      params do
+        requires :id, type: Integer, desc: "#{api_resource} id."
+      end
+      get ":id" do
+        { :action => "#{api_resource} action" }
+      end
     end
   end
 end
