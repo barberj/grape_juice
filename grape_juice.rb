ac4a1021131e2779a9b4b301cd1995d5ffb3c564
Justin Barber <barber.justin@gmail.com>
2015-07-23 21:42:50 -0400

    more meta magic

--- grape_juice.rb
+++ grape_juice.rb
@@ -2,6 +2,25 @@ require './grape_juice_v1'
 require './grape_juice_v2'
 require 'pry'
 
+GrapeJuiceV3 = Class.new Grape::API
+
+GrapeJuiceV3.format :json
+GrapeJuiceV3.version 'v3', using: :path
+[:users, :statuses].each do |api_resource|
+  GrapeJuiceV3.resource api_resource do
+    GrapeJuiceV3.get do
+      { :action => "#{api_resource} v3 index" }
+    end
+
+    GrapeJuiceV3.params do
+      requires :id, type: Integer, desc: "#{api_resource} id."
+    end
+    GrapeJuiceV3.get ":id" do
+      { :action => "#{api_resource} v3 action" }
+    end
+  end
+end
+
 class GrapeJuice < Grape::API
   ObjectSpace.each_object(::Class).each do |klass|
     next if [GrapeJuice, Grape::API].include? klass
