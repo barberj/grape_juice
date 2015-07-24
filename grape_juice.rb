1483e3eb185f5ff3883fca5a408ce1068981f5c1
Justin Barber <barber.justin@gmail.com>
2015-07-23 20:52:20 -0400

    example of how to incorrectly version

--- grape_juice.rb
+++ grape_juice.rb
@@ -16,4 +16,19 @@ class GrapeJuice < Grape::API
       { :action => 'read' }
     end
   end
+
+  version 'v2'
+
+  resource :users do
+    get do
+      { :action => 'v2_index' }
+    end
+
+    params do
+      requires :id, type: Integer, desc: "User id."
+    end
+    get ":id" do
+      { :action => 'v2_read' }
+    end
+  end
 end
